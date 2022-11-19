9c39b56c13e5b724b3a4f0008308630643ab3402
gamedevCloudy <acaayush1111@gmail.com>
2022-11-20 00:18:40 +0530

    Python Linked List

---
+++ linkedlist.py
@@ -0,0 +1,78 @@
+class Node(object):
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+    
+    def GetData(self): 
+        return self.val
+    
+    def GetNext(self): 
+        return self.next
+    
+    def SetValue(self,val): 
+        self.val = val
+    
+    def SetNext(self, next): 
+        self.next = next
+    
+
+class CreateLinkedList(object): 
+    current = Node(0)
+    def __init__(self, head):
+        self.head = head
+        self.count = 0
+        
+
+    def Insert(self, data): 
+        newNode = Node(data) #creates a new node with Data as the value
+        newNode.SetNext(self.head) #sets the next of the new node to head
+
+        self.head = newNode # sets the head as the new node
+        # self.current = self.head
+
+        self.count +=1
+    
+    def InsertNext(self, data): 
+        newNode = Node(data)
+
+        if(self.head != None):
+            self.current.SetNext(newNode)
+            self.current = newNode 
+        else: self.insert(data)
+    
+    def PrintList(self):
+        temp = self.current
+        self.current = self.head
+        while(self.current.GetNext() != None): 
+            print(self.current.GetData())
+            self.current = self.current.GetNext()
+        self.current.GetData()
+        self.current = temp 
+
+
+    
+
+def Main(): 
+    print("\nCreate a new linked list: ")
+    x = input("Enter first element: ")
+    y = CreateLinkedList(5)
+    y.Insert(x)
+
+    z = input("\n How many more elements do you want to add: ")
+    for i in z:
+        m = input("\n Next Element: ")
+        y.InsertNext(m)
+
+    print("\nPrinting entire list: ") 
+    y.PrintList()
+
+Main()
+
+
+
+
+             
+        
+
+
+    
