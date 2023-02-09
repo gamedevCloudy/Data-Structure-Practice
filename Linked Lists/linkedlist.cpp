dc63cc3e0d3e776ddbf3a7b7058cbbf953d09cbb
gamedevCloudy <acaayush1111@gmail.com>
2023-02-09 18:48:27 +0530

    Update
    - Removed Old folders, organised
    - Linked List in Cpp

---
+++ Linked Lists/linkedlist.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+
+using namespace std; 
+
+struct Node
+{
+    int val; 
+    Node* next; 
+};
+class LinkedList
+{
+    private:Node* head;
+            Node* current; 
+
+    public:
+            LinkedList()
+            {
+                head=NULL;
+                current=NULL;
+            }
+
+            LinkedList(int headVal)
+            {
+                head->val=headVal; 
+                current=head;
+            }
+
+            void AddNode(int val)
+            {
+                Node* temp = new Node(); //do not forget to initialize this with new!!!
+                temp->val=val;
+
+                if(head==NULL)
+                {
+                    head=temp;
+                    current=head;
+                }
+                else
+                {
+                    current->next=temp; 
+                    current=temp; 
+                }
+            }
+
+            void RemoveNodeByValue(int val)
+            {
+                Node* before=head;
+                Node*target=head;
+                Node* after;
+
+                
+                while(target->val!= val)
+                {
+                    target=target->next;
+                }
+                while (before->next!=target and target!=head)
+                {
+                    before=before->next;
+                }
+                after=target->next;
+
+                if(before==target)
+                {
+                    head=after;
+                }
+                else{
+                    before->next=after; 
+                }
+                delete  target;
+                
+            }
+            void PrintList()
+            {
+                Node*temp=(head);
+
+                do
+                {
+                   cout<<temp->val<<"\t";
+                   temp=temp->next; 
+                } while (temp->next!=NULL);
+                cout<<temp->val<<endl; 
+            }
+
+            int Size()
+            {
+                int count; 
+                Node*temp=(head);
+                do
+                {
+                   count++;
+                   temp=temp->next; 
+                } while (temp->next!=NULL);
+                return count;
+            }
+
+            void ClearList()
+            {
+                Node* crt = head;
+                Node* next = NULL;
+            
+                while (crt != NULL)
+                {
+                    next = crt->next;
+                    delete crt;
+                    crt = next;
+                }
+            
+                
+                head = NULL;
+                current=NULL;
+            }
+
+}; 
+
+int main()
+{
+    LinkedList l0; 
+    l0.AddNode(6);
+    l0.AddNode(26);
+    l0.AddNode(2);
+    l0.PrintList();
+    cout<<"\n"<<l0.Size()<<endl;
+
+    
+    l0.RemoveNodeByValue(6);
+    l0.PrintList();
+    cout<<"\n"<<l0.Size()<<endl;
+    
+    l0.ClearList();
+    return 0; 
+}
